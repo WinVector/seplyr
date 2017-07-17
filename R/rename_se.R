d3cdea092ad61a9a6b93483827c41cf060ce687f
John Mount <jmount@win-vector.com>
2017-07-16 18:58:52 -0700

    update doc

--- R/rename_se.R
+++ R/rename_se.R
@@ -6,17 +6,27 @@
 #' @seealso \code{\link[dplyr]{rename}},  \code{\link[dplyr]{rename_at}}
 #'
 #' @param .data data.frame
-#' @param mapping named character vector of columns to rename (new news on the left, original names on the right).
+#' @param mapping named character vector of columns to rename (new news on the left, original names on the right; this may seem reversed but it matches dplyr::rename()).
 #' @return .data with renamed columns
 #'
 #' @examples
 #'
 #' datasets::mtcars %>%
-#'    rename_se(c("cylinders" = "cyl", "gears" = "gear")) %>% head()
+#'    rename_se(c("cylinders" = "cyl", "gears" = "gear")) %>%
+#'    head()
+#' # # sames as:
+#' # datasets::mtcars %>%
+#' #    rename(cylinders = cyl, gears = gear) %>%
+#' #    head()
 #'
 #' @export
 #'
 rename_se <- function(.data, mapping) {
   mp <- lapply(mapping, rlang::sym)
-  do.call(rename, c(list(.data) , mp))
+  return(do.call(rename, c(list(.data) , mp)))
+  # # alternate impl
+  # mp <- rlang::syms(names(mapping))
+  # names(mp) <- as.character(mapping)
+  # mapfn <- function(ci) { mp[ci] }
+  # return(rename_at(.tbl = .data, .vars = names(mp), mapfn))
 }
