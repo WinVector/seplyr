0dc6c8f0ef2a06f1005e987ac2d43de524adf18b
John Mount <jmount@win-vector.com>
2017-07-23 08:33:19 -0700

    change := to an S3 method

--- R/namedMapBuilder.R
+++ R/namedMapBuilder.R
@@ -1,3 +1,5 @@
+
+
 #' Named map builder.
 #'
 #' Set names of right-argument to be left-argument, and return right argument.
@@ -6,27 +8,44 @@
 #' @param values values to assign names to (and return).
 #' @return values with names set.
 #'
-#'
-#' @aliases named_map_builder
-#'
-#'
 #' @examples
 #'
 #'
 #' c('a' := '4', 'b' := '5')
 #' # equivalent to: c(a = '4', b = '5')
 #'
-#' c('a', 'b') := c(1, 2)
-#' # equivalent to: c(a = 1, b = 2)
+#' c('a', 'b') := c('1', '2')
+#' # equivalent to: c(a = '1', b = '2')
 #'
 #' # the important example
 #' name <- 'a'
-#' name := 5
-#' # equivalent to: c("a" = 5)
+#' name := '5'
+#' # equivalent to: c('a' = '5')
 #'
 #' @export
-`:=` <- function(names, values) {
-  names(values) <- names
+named_map_builder <- function(names, values) {
+  names(values) <- as.character(names)
   values
 }
+
+#' @rdname named_map_builder
+#' @export
+`:=` <- function(names, values) UseMethod(":=")
+
+# override as few S3 types as we reasonably need.
+# deliberaterly leave default alone
+# as a "good citizen".
+
+# #' @export
+# `:=.default` <- named_map_builder
+
+#' @export
+`:=.character` <- named_map_builder
+
+#' @export
+`:=.list` <- named_map_builder
+
+#' @export
+`:=.name` <- named_map_builder
+
 
