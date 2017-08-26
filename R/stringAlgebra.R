ffd5c5870169d0b387a6176a3938d5e1f631c0d2
John Mount <jmount@win-vector.com>
2017-08-26 12:35:56 -0700

    working on string expansion.  Need a bunch of small tests to document intent.

--- R/stringAlgebra.R
+++ R/stringAlgebra.R
@@ -1,28 +1,93 @@
 
-
-# remove one level of quoting from a string
-dequote_str <- function(nexpr, env) {
-  # look for "'x'" forms, if so strip off one level of quotes
-  nc <- nchar(nexpr)
-  regexp <- '[^\\]([\\][\\])*[\\][\'|"]$'
+#' Check if a string is quoted.
+#'
+#' @param str string to work with (not vectorized).
+#' @return TRUE if string is quoted, else FALSE.
+#'
+#' @noRd
+#'
+is_quoted <- function(str) {
+  # look for "'x'" forms
+  nc <- nchar(str)
+  if(nc<3) {
+    return(FALSE)
+  }
+  if(!(substr(str,1,1) %in% c('"', "'"))) {
+    return(FALSE)
+  }
+  if(substr(str,1,1)!=substr(str,nc,nc)) {
+    return(FALSE)
+  }
   # avoid last quote escaped situation
+  # odd number of escapes prior to putative close quote.
   #  grep(regexp, '"\\\\"') # integer(0)
   #  grep(regexp, '"\\"') # integer(1)
-  if((nc>=3) && (substr(nexpr,1,1)==substr(nexpr,nc,nc)) &&
-     (substr(nexpr,1,1) %in% c('"', "'")) &&
-     (length(grep(regexp,nexpr)==0))) {
-    # exception an odd number of escapes before the last quote
-    return(substr(nexpr,2,nc-1))
+  regexp <- '[^\\]([\\][\\])*[\\][\'|"]$'
+  if(length(grep(regexp,str)==0)) {
+    return(FALSE)
   }
+  return(TRUE)
+}
+
+#' Remove one level of indirection from a string or name.
+#'
+#' Needs to return symbols to prevent re-quoting of strings.
+#'
+#' @param symb string or symbol to work with (not vectorized).
+#' @param env environment to look in
+#' @return de-quoted symb (or originval if not quoted).
+#'
+#' @noRd
+#'
+deref_symb <- function(symb, env) {
+  #  look for a dereference opportunity
   val <- NULL
   tryCatch(
-    val <- get(nexpr, envir = env),
+    val <- get(as.character(symb), envir = env),
     error = function(e) { NULL }
   )
-  if((!is.null(val)) && (is.character(val)) && (length(val)==1)) {
-    return(as.symbol(val))
+  if((!is.null(val))&&(length(val)==1)) {
+    if(is.name(val)) {
+      return(val)
+    }
+    if(is.character(val)) {
+      if(is_quoted(val)) {
+        return(substr(val,2,nchar(val)-1)) # strip of quotes, but leave as string
+      } else {
+        return(as.name(val))
+      }
+    }
   }
-  return(as.symbol(nexpr))
+  # return original with name xform to prevent quote growth
+  if(is.name(symb)) {
+    return(symb)
+  }
+  # character
+  if(is_quoted(symb)) {
+    return(substr(symb,2,nchar(symb)-1)) # strip of quotes, but leave as string
+  } else {
+    return(as.name(symb))
+  }
+}
+
+#' Remove one level of quoting or one level of indirection from a string.
+#'
+#' Needs to return symbols to prevent re-quoting of strings.
+#'
+#' @param str string to work with (not vectorized).
+#' @param env environment to look in
+#' @return de-quoted str (or originval if not quoted).
+#'
+#' @noRd
+#'
+deref_str <- function(str, env) {
+  # look for "'x'" forms, if so strip off one level of quotes
+  if(is_quoted(str)) {
+    str <- substr(str,2,nchar(str)-1)
+    return(str)
+  }
+  # now look for a dereference opportunity
+  return(deref_symb(str,env))
 }
 
 #' Substitute language elements by one-expand_expr.
@@ -47,7 +112,7 @@ prep_deref <- function(lexpr, env) {
     for(i in seq_len(length(nms))) {
       ki <- as.character(nms[[i]])
       if(length(ki)>0) {
-        ri <- as.character(dequote_str(ki, env))
+        ri <- deref_str(ki, env)
         if((length(ri)>0)&&(ri!=ki)) {
           nms[[i]] <- ri
         }
@@ -62,26 +127,25 @@ prep_deref <- function(lexpr, env) {
     }
     return(nexpr)
   }
-  # try to strip quotes off, producing either a symbol or string
+  # try to strip quotes off and deref
   if(is.character(nexpr)) {
-    return(dequote_str(nexpr, env))
+    return(deref_str(nexpr, env))
   }
-  if(is.symbol(nexpr)) { # same as is.name()
-    val <- NULL
-    tryCatch(
-      val <- get(as.character(nexpr), envir = env),
-      error = function(e) { NULL }
-    )
-    if((!is.null(val)) && (is.character(val)) && (length(val)==1)) {
-      return(as.symbol(val))
-    }
-    return(nexpr)
+  if(is.name(nexpr)) {
+    return(deref_symb(nexpr, env))
   }
   # fallback
   return(nexpr)
 }
 
 #' Prepare an expression for standard evaluation.
+#'
+#' Combine string-variable values and quoted terms to produce
+#' a concrete string reading for value orietend string evaluation
+#' (stringified standard evaluation).  We also call this the
+#' string algebra.  This method powers the \code{seplyr::*_nse()}
+#' methods and helps build string expressions without needing
+#' \code{paste0())} or \code{glue::glue()}.
 #'
 #' @param expr to de-ref.
 #' @param env enviornment in.
